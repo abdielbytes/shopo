a6fa5a4c5c11633aa7c9028c640ff256ce62d0a5
abdielbytes <wilsonabdiel86@gmail.com>
2025-01-27 19:41:21 +0100

    erer

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Str;
-
 
 class User extends Authenticatable
 {
@@ -47,18 +45,4 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-    protected $keyType = 'string';
-    public $incrementing = false;
-
-    
-    protected static function boot()
-    {
-        parent::boot();
-        static::creating(function ($model) {
-            if (empty($model->{$model->getKeyName()})) {
-                $model->{$model->getKeyName()} = (string) Str::uuid();
-            }
-        });
-    }
-    
 }
